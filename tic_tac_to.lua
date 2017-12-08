a5085251e66eaa42b38b99d541eb0bad77f074e8
paul <paulstuartparker@gmail.com>
2017-12-08 02:06:43 -0500

    add move functionality"

--- tic_tac_to.lua
+++ tic_tac_to.lua
@@ -1,7 +1,6 @@
-print("hellow world")
-
-PLAYER_1 = "x"
-PLAYER_2 = "o"
+PLAYER_1 = {type="human", symbol="x"}
+PLAYER_2 = {type="human", symbol="o"}
+current_player = PLAYER_1
 EMPTY_SQUARE = " "
 GRID = {}
 GRID[1] = {"| ___ |", " ___ |", " ___ |"}
@@ -15,11 +14,10 @@ board[2] = {" ", " ", " "}
 board[3] = {" ", " ", " "}
 
 function greet()
-  io.write("Welcome to tic-tac-toe! To move, enter the row you would like to move to \nfollowed by a comma then space, then the column you would like to move to. \ne.g: 1, 2 \n")
+  io.write("Welcome to tic-tac-toe! \nTo move, enter the row you would like to move to followed by a comma then space, then the column you would like to move to. \ne.g: 1, 2 \n")
 end
 
 function render(a_board)
-
   io.write("    1     2     3     ")
   print("")
   for i = 1, 3, 1 do
@@ -50,19 +48,90 @@ function render(a_board)
   end
 end
 
-function play_turn()
-  x, y = get_user_input()
+function move_piece(x, y)
+  print "moving"
+  board[x][y] = current_player.symbol
+  print(board[x][y])
 end
 
+function valid_move(x, y)
+  if (x < 4 and x > 0) and (y < 4 and y > 0) then
+    if board[x][y] ~= " " then
+      return false
+    end
+    return true
+  end
+  return false
+end
+
+
+function swap()
+  if current_player == PLAYER_1 then
+    current_player = PLAYER_2
+  else
+    current_player = PLAYER_1
+  end
+end
+
+function play_turn()
+  moved = false
+  while (moved == false) do
+    if current_player.type == "human" then
+      x, y = get_user_input()
+    else
+      x, y = get_computer_input()
+    end
+    if valid_move(x, y) then
+      move_piece(x, y)
+      x, y = nil, nil
+      moved = true
+      swap()
+    end
+  end
+end
+
+
+function parse(x, y)
+  if string.len(x) == 1 and string.len(y) == 1 then
+
+    x = tonumber(x)
+    y = tonumber(y)
+    return x, y
+  end
+  return false
+end
+
+function won()
+  return false
+end
 
 function get_user_input()
-
+  valid = false
+  while valid == false do
+    os.execute("clear")
+    render(board)
+    io.write("Please enter the row where you want to play.\n")
+    x = io.read()
+    io.write("Please enter the column where you want to play.\n")
+    y = io.read()
+    if parse(x, y) then
+      x, y = parse(x, y)
+      print(valid)
+      valid = true
+      -- print(valid)
+      -- print(current_player.symbol)
+    end
+  end
+  return x, y
 end
-
 
 function play_game()
   greet()
   render(board)
+  while (won() == false) do
+    play_turn()
+  end
+  io.write(current_player, "wins!!!\n")
 end
 
 -- render(board)
